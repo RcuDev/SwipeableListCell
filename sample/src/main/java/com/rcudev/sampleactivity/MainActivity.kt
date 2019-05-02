f01be7c588438fa75953518d7be02df80e933aca
Raul Corvo <rcu1987@gmail.com>
2019-05-02 13:02:51 +0200

    initial commit

---
+++ sample/src/main/java/com/rcudev/sampleactivity/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.rcudev.sampleactivity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ swipeablelistcell/src/main/java/com/rcudev/swipeablelistcell/SwipeableListCell.kt
@@ -0,0 +1,14 @@
+package com.rcudev.swipeablelistcell
+
+import android.content.Context
+import android.support.constraint.ConstraintLayout
+import android.util.AttributeSet
+
+class SwipeableListCell @JvmOverloads constructor(context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0)
+    : ConstraintLayout(context, attrs, defStyleAttr) {
+
+    init {
+
+    }
+
+}
